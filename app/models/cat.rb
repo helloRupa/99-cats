07f8266cf524ffa2a0241163531ed6afbdfb22b8
Rupa Dhillon <rupa211984@gmail.com>
2019-04-26 15:59:52 -0700

    Add Cat migration and Model

---
+++ app/models/cat.rb
@@ -0,0 +1,16 @@
+require 'action_view'
+require 'action_view/helpers'
+
+class Cat < ApplicationRecord
+  include ActionView::Helpers::DateHelper
+
+  COLORS = %w[black white grey ginger blue brown calico]
+
+  validates :birth_date, :color, :name, :sex, :description, presence: true
+  validates :color, inclusion: { in: COLORS, message: "%{value} is not a valid color" }
+  validates :sex, inclusion: { in: %w[M F], message: 'Must be M or F' }
+
+  def age
+    time_ago_in_words(self.birth_date)
+  end
+end
