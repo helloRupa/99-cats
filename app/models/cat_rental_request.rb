167b51f96aed271e1eeb00bc8d2781f3f40cc10d
Rupa Dhillon <rupa211984@gmail.com>
2019-05-03 00:08:05 -0700

    Privatize some model methods

--- app/models/cat_rental_request.rb
+++ app/models/cat_rental_request.rb
@@ -5,39 +5,6 @@ class CatRentalRequest < ApplicationRecord
 
   belongs_to :cat
   belongs_to :user
-
-  def valid_dates
-    valid = !self.start_date.nil? && !self.end_date.nil? && self.start_date >= Date.current && self.start_date <= self.end_date
-    msg = 'Dates must be today or in the future'
-    errors.add(:start_date, message: msg) unless valid
-  end
-  
-  def overlapping_requests
-    CatRentalRequest.where(cat_id: self.cat_id)
-      .where.not(id: self.id)
-      .where.not('start_date > ? AND start_date > ? OR end_date < ?', self.start_date, self.end_date, self.start_date)
-  end
-
-  def overlapping_approved_requests
-    overlapping_requests.where(status: 'APPROVED')
-  end
-
-  def overlapping_pending_requests
-    overlapping_requests.where(status: 'PENDING')
-  end
-
-  def does_not_overlap_approved_request
-    return unless pending?
-
-    errors.add(:start_date, message: 'Cat unavailable on those dates') if overlapping_approved_requests.exists?
-  end
-
-  def deny_overlapping!
-    overlapping_pending_requests.each do |req|
-      req.status = 'DENIED'
-      req.save!
-    end
-  end
   
   def approve!
     raise 'Not Pending' unless pending?
@@ -59,5 +26,38 @@ class CatRentalRequest < ApplicationRecord
 
   def pending?
     self.status == 'PENDING'
+  end
+
+  def overlapping_requests
+    CatRentalRequest.where(cat_id: self.cat_id)
+      .where.not(id: self.id)
+      .where.not('start_date > ? AND start_date > ? OR end_date < ?', self.start_date, self.end_date, self.start_date)
+  end
+
+  def overlapping_approved_requests
+    overlapping_requests.where(status: 'APPROVED')
+  end
+
+  def overlapping_pending_requests
+    overlapping_requests.where(status: 'PENDING')
+  end
+
+  def deny_overlapping!
+    overlapping_pending_requests.each do |req|
+      req.status = 'DENIED'
+      req.save!
+    end
+  end
+
+  def valid_dates
+    valid = !self.start_date.nil? && !self.end_date.nil? && self.start_date >= Date.current && self.start_date <= self.end_date
+    msg = 'Dates must be today or in the future'
+    errors.add(:start_date, message: msg) unless valid
+  end
+
+  def does_not_overlap_approved_request
+    return unless pending?
+
+    errors.add(:start_date, message: 'Cat unavailable on those dates') if overlapping_approved_requests.exists?
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -29,13 +29,15 @@ class User < ApplicationRecord
     SecureRandom::urlsafe_base64(16)
   end
 
-  def ensure_session_token
-    self.session_token ||= self.class.generate_session_token
-  end
-
   def reset_session_token!
     self.session_token = self.class.generate_session_token
     self.save!
     self.session_token
+  end
+
+  private
+
+  def ensure_session_token
+    self.session_token ||= self.class.generate_session_token
   end
 end
